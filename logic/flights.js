5778a8901879357b6d7489ed5e1d947a581b80ad
Julio Quezada <julio.quezada@catolica.edu.sv>
2022-12-28 20:43:50 -0600

    GettingStarted

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,12 @@
 function Flights() {
 
+    calculateNumberOfFlights(passengers, capacity) {
+        let flights;
+
+        if (passengers % capacity) {
+            flights = (passengers / capacity) + 1;
+        }
+    }
+
 }
 
